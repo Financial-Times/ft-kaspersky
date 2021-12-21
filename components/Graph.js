dd555d7c0121dc532bd45bbf39fc82225a683dae
Niv Suresh <nivsuresh06@gmail.com>
2021-12-21 14:32:47 +0000

    Update Graph.js

--- components/Graph.js
+++ components/Graph.js
@@ -18,6 +18,7 @@ const Graph = ({ data }) => {
 				data-title={data.title}
 			></div>
 			<Script
+				id="grpahId"
 				dangerouslySetInnerHTML={{
 					__html: `!function(e,i,n,s){var t="InfogramEmbeds",d=e.getElementsByTagName("script")[0];if(window[t]&&window[t].initialized)window[t].process&&window[t].process();else if(!e.getElementById(n)){var o=e.createElement("script");o.async=1,o.id=n,o.src="https://e.infogram.com/js/dist/embed-loader-min.js",d.parentNode.insertBefore(o,d)}}(document,0,"infogram-async");`,
 				}}
