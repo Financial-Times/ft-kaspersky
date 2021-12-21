6c10bbee81b66fc673e925e49b3cee85632411b2
Niv Suresh <nivsuresh06@gmail.com>
2021-12-21 14:28:45 +0000

    Fixing Script issue

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -54,23 +54,26 @@ export default function ArticlePage({ post, related }) {
 				<title>{post.title}</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				{/* <Metadata data={post.metaData} /> */}
-				<Script
-					dangerouslySetInnerHTML={{
-						__html: `
+			</Head>
+			<Script
+				id="permutive"
+				dangerouslySetInnerHTML={{
+					__html: `
           !function (n, e, o, r, i) { if (!e) { e = e || {}, window.permutive = e, e.q = [], e.config = i || {}, e.config.projectId = o, e.config.apiKey = r, e.config.environment = e.config.environment || "production"; for (var t = ["addon", "identify", "track", "trigger", "query", "segment", "segments", "ready", "on", "once", "user", "consent"], c = 0; c < t.length; c++) { var f = t[c]; e[f] = function (n) { return function () { var o = Array.prototype.slice.call(arguments, 0); e.q.push({ functionName: n, arguments: o }) } }(f) } } }(document, window.permutive, "e1c3fd73-dd41-4abd-b80b-4278d52bf7aa", "b2b3b748-e1f6-4bd5-b2f2-26debc8075a3", { "consentRequired": true });
           window.googletag = window.googletag || {}, window.googletag.cmd = window.googletag.cmd || [], window.googletag.cmd.push(function () { if (0 === window.googletag.pubads().getTargeting("permutive").length) { var g = window.localStorage.getItem("_pdfps"); window.googletag.pubads().setTargeting("permutive", g ? JSON.parse(g) : []) } });
           window.permutive.addon('web', {
             page: {
-              type: 'Partner Content ${post.metaData.hasVideo ? 'Video' : 'Article'}',
+              type: 'Partner Content ${
+								post.metaData.hasVideo ? 'Video' : 'Article'
+							}',
             }
           });`,
-					}}
-				></Script>
-				<Script
-					async
-					src="https://e1c3fd73-dd41-4abd-b80b-4278d52bf7aa.edge.permutive.app/e1c3fd73-dd41-4abd-b80b-4278d52bf7aa-web.js"
-				></Script>
-			</Head>
+				}}
+			></Script>
+			<Script
+				async
+				src="https://e1c3fd73-dd41-4abd-b80b-4278d52bf7aa.edge.permutive.app/e1c3fd73-dd41-4abd-b80b-4278d52bf7aa-web.js"
+			></Script>
 		</>
 	);
 }
