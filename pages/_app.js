edff0dd20f5cd539320a870873dc899bf9ba7815
Niv Suresh <nivsuresh06@gmail.com>
2022-01-18 16:37:52 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
-import Layout from '../components/Layout';
-import ReactGA from 'react-ga';
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import Layout from "../components/Layout";
+import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps, router }) {
-  ReactGA.initialize('UA-93924635-35');
+  ReactGA.initialize("UA-93924635-43");
 
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-T66ZTL5' });
+    TagManager.initialize({ gtmId: "GTM-T66ZTL5" });
   }, []);
 
   useEffect(() => {
     ReactGA.pageview(document.location.href);
-    router.events.on('routeChangeComplete', () => {
+    router.events.on("routeChangeComplete", () => {
       ReactGA.pageview(document.location.href);
     });
 
     return () => {
-      router.events.off('routeChangeComplete', ReactGA.pageview(router.asPath));
+      router.events.off("routeChangeComplete", ReactGA.pageview(router.asPath));
     };
   }, [router]);
 
